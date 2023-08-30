1eadac1f91c0db39e08ded10cd4a0185445c3647
Kunal Kirimkar <61002619+KunalKirimkar@users.noreply.github.com>
2023-08-31 00:16:10 +0530

    [Added] Navigation bar and frontend sample of disease detection fragment.

--- TomatoDiseaseDetectionApp/app/src/main/java/com/kunalkirimkar/tomatodiseasedetectionapp/MainActivity.java
+++ TomatoDiseaseDetectionApp/app/src/main/java/com/kunalkirimkar/tomatodiseasedetectionapp/MainActivity.java
@@ -1,37 +1,77 @@
 package com.kunalkirimkar.tomatodiseasedetectionapp;
 
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.fragment.app.FragmentTransaction;
 
-import com.kunalkirimkar.tomatodiseasedetectionapp.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
+import com.kunalkirimkar.tomatodiseasedetectionapp.fragments.DiseaseDetectionFragment;
+import com.kunalkirimkar.tomatodiseasedetectionapp.fragments.MarketIntelligenceFragment;
+import com.kunalkirimkar.tomatodiseasedetectionapp.fragments.PreservationFragment;
+import com.kunalkirimkar.tomatodiseasedetectionapp.fragments.ProductionFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.production);
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return changeFragment(item.getItemId());
+            }
+        });
+
+        changeFragment(R.id.production);
     }
 
+    private boolean changeFragment(int itemId) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        if (itemId == R.id.production){
+            fragmentTransaction.replace(R.id.container, new ProductionFragment());
+        } else if (itemId == R.id.diseaseDetection) {
+            fragmentTransaction.replace(R.id.container, new DiseaseDetectionFragment());
+        } else if (itemId == R.id.marketIntelligence) {
+            fragmentTransaction.replace(R.id.container, new MarketIntelligenceFragment());
+        } else {
+            fragmentTransaction.replace(R.id.container, new PreservationFragment());
+        }
+
+        fragmentTransaction.addToBackStack(null).commit();
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 200) {
+
+            // Checking whether user granted the permission or not.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // Showing the toast message
+                Toast.makeText(MainActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
